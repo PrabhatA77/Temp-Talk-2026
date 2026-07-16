0402553bd8af9d1932f30937f893832556c8e259
Prabhat <testops177@gmail.com>
2026-07-16 17:28:14 +0530

    changed deploy setting

--- Server/src/Controllers/authController.js
+++ Server/src/Controllers/authController.js
@@ -249,7 +249,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
     });
 
@@ -320,7 +320,7 @@ export const googleLogin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
